c52c005849a4a56976f6ea60df76447e399b39fc
Bernard Debecker <bernard@fanduel.com>
2015-03-20 23:27:49 +0000

    New example, a bit more practical and realist

--- example/example.js
+++ example/example.js
@@ -1,42 +1,19 @@
 
-function modDivs() {
+function setThings() {
   var sheet = new Sheet();
+  var colorSet = {};
+  var input = document.getElementById('color-input');
+  input.onkeyup = changeColor;
 
-  var set = {};
-  set.width = '100px';
-  set.position = 'absolute';
-  set.height = '100px';
-  set.top = '100px';
-  set.left = '100px';
-  set.backgroundColor = 'red';
-  set.paddingTop = '10px';
+  function changeColor(event) {
+    var hex = input.value;
+    var regex = /^#(?:[0-9a-fA-F]{3}){1,2}$/;
+    if (hex.match(regex)) {
+      colorSet.color = hex;
+      colorSet.borderColor = hex;
+      sheet.updateSet('.user-defined', colorSet);
+    }
+  }
 
-  sheet.add('.test', set);
-
-  var set2 = {};
-  set2.width = '100px';
-  set2.position = 'absolute';
-  set2.height = '100px';
-  set2.top = '300px';
-  set2.left = '300px';
-  set2.backgroundColor = 'green';
-  set2.paddingTop = '10px';
-  set2.transition = 'all 0.5s ease-in-out';
-
-  sheet.add('.test2', set2);
-
-  set2.width = '200px';
-  set2.position = 'absolute';
-  set2.height = '200px';
-  set2.top = '250px';
-  set2.left = '250px';
-  set2.backgroundColor = 'blue';
-  set2.color = 'white';
-  set2.paddingTop = '20px';
-
-  setTimeout(function() {
-    sheet.updateSet('.test2', set2);
-  }, 2000);
 }
-
-window.onload = modDivs;
+window.onload = setThings;
